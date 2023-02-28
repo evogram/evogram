0e020f5b5afae21d932dac3a779e89826affea85
Велокс <czvelox@gmail.com>
2023-02-28 05:50:19 +0300

    Support custom contexts in buildIn contexts

--- src/modules/context/Context.ts
+++ src/modules/context/Context.ts
@@ -2,4 +2,17 @@ import { Evogram } from "../../Client";
 
 export class Context<T> {
 	constructor(protected client: Evogram, public source: T) {}
+	/**
+	 * Converts the source object to a context object of a given type.
+	 * @template TContext - Generic type parameter for the context object.
+	 * @param {any} source - The source object to convert.
+	 * @returns {TContext} The context object.
+	 * 
+	 * @example
+	 * 	client.updates.on("message", message => {
+	 * 		const user = message.toContext<CustomUserContext>(message.user);
+	 * 		// Now user works correctly with your context
+	 * 	})
+	 */
+	public toContext<TContext extends object>(source: any): TContext { return source }
 }
