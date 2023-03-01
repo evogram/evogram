1db24c3dd8ebef6df83ef257bd45c834bf583029
Велокс <czvelox@gmail.com>
2023-03-01 20:41:14 +0300

    Change toContext logic

--- src/modules/context/Context.ts
+++ src/modules/context/Context.ts
@@ -5,14 +5,16 @@ export class Context<T> {
 	/**
 	 * Converts the source object to a context object of a given type.
 	 * @template TContext - Generic type parameter for the context object.
-	 * @param {any} source - The source object to convert.
 	 * @returns {TContext} The context object.
 	 * 
 	 * @example
 	 * 	client.updates.on("message", message => {
-	 * 		const user = message.toContext<CustomUserContext>(message.user);
+	 * 		const user = message.user.toContext<CustomUserContext>();
 	 * 		// Now user works correctly with your context
 	 * 	})
 	 */
-	public toContext<TContext extends object>(source: any): TContext { return source }
+	public toContext<TContext extends object>(): TContext {
+		//@ts-ignore
+		return this._source 
+	}
 }
